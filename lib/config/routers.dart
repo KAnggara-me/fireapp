346612f9930cc36d5077c6e7e42cf15110ae86d6
KAnggara <kanggara75@gmail.com>
2022-02-05 15:30:13 +0700

    add: routers content

--- lib/config/routers.dart
+++ lib/config/routers.dart
@@ -1,4 +1,14 @@
+import '../controllers/c_auth.dart';
+import '../controllers/c_navbar.dart';
+import '../controllers/c_splash.dart';
 import 'package:flutter/widgets.dart';
+import '../controllers/c_setting.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  UserScreen.routeName: (context) => const UserScreen(),
+  SplashPage.routeName: (context) => const SplashPage(),
+  AdminScreen.routeName: (context) => const AdminScreen(),
+  LoginScreen.routeName: (context) => const LoginScreen(),
+  UserSettingScreen.routeName: (context) => const UserSettingScreen(),
+  AdminSettingScreen.routeName: (context) => const AdminSettingScreen(),
 };
