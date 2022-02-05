fc83e5b48859fa803373baa7b1da807193e213e7
KAnggara <kanggara75@gmail.com>
2022-02-05 15:29:10 +0700

    fix: use MVC pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:fire_app/constant/text.dart';
-import 'package:fire_app/views/board/v_board.dart';
-// import 'package:fire_app/views/device/v_devices.dart';
+import './config/themes.dart';
+import './constant/text.dart';
+import './config/routers.dart';
+import './controllers/c_splash.dart';
 import 'package:flutter/material.dart';
-import 'config/themes.dart';
-// import './config/routers.dart';
 
 void main() {
   runApp(
@@ -13,17 +12,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //Disable Debud banner on right top app
       title: kAppName,
       theme: theme(), //Theme for this App
-      // initialRoute: SplashScreen, //Redirect to Splash Screen
-      // routes: routes, //Setup Routes from routers.dart
-      // home: const DevicePage(), //Redirect to Splash Screen
-      home: const BoardPage(),
+      initialRoute: SplashPage.routeName, //Redirect to Splash Screen
+      routes: routes, //Setup Routes from routers.dart
     );
   }
 }
