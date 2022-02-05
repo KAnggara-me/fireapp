7c35f76c2918449955f2f84d3ceb504d7742ef7c
KAnggara <kanggara75@gmail.com>
2022-02-05 15:34:21 +0700

    add: login

---
+++ lib/views/auth/v_login.dart
@@ -0,0 +1,48 @@
+import 'package:fire_app/constant/size.dart';
+import 'package:fire_app/views/auth/v_login_form.dart';
+import 'package:flutter/material.dart';
+
+class LoginBody extends StatelessWidget {
+  const LoginBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(25)!,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.05),
+                Image.asset(
+                  "assets/img/logo.png",
+                  height: SizeConfig.screenHeight * 0.25,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.02,
+                ),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.03,
+                ),
+                const Login(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/auth/v_login_form.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: avoid_print
+
+import 'package:fire_app/constant/colors.dart';
+import 'package:fire_app/constant/size.dart';
+import 'package:fire_app/controllers/c_auth.dart';
+import 'package:fire_app/helpers/keyboard.dart';
+import 'package:fire_app/widgets/w_button.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends LoginController {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          usernameField(),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          passwordField(),
+          SizedBox(
+            height: getProportionateScreenHeight(5),
+          ),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (i) {
+                  setState(() {
+                    remember = i!;
+                    print(i);
+                  });
+                },
+              ),
+              Text(
+                "Remember me",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(12.5),
+                ),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () => forgot(),
+                child: Text(
+                  "Forgot Password",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontSize: getProportionateScreenWidth(12.5),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Text(
+            msg,
+            style: const TextStyle(fontSize: 20.0, color: Colors.red),
+          ),
+          LoginButton(
+            text: "Login",
+            press: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                KeyboardUtil.hideKeyboard(context);
+                cek();
+              }
+            },
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(10),
+          ),
+        ],
+      ),
+    );
+  }
+}
