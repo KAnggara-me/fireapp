ed55ab8e8943988bafa40bbb022efedc308329ce
KAnggara <kanggara75@gmail.com>
2022-02-05 15:31:51 +0700

    add: url, remove unused

--- lib/constant/url.dart
+++ lib/constant/url.dart
@@ -1,11 +1,20 @@
 import 'package:http/http.dart' as http;
 
 class BaseUrl {
-  // static String baseUrl = "https://fireapp.kahosting.my.id/api/";
-  static String baseUrl = "http://fire-api.test/api/";
+  static String baseUrl = "https://fireapp.kahosting.my.id/api/";
   static String log = baseUrl + "log";
   static String board = baseUrl + "board";
   static String sensor = baseUrl + "sensor";
+  static String login = baseUrl + "auth";
+  static String image = "https://source.unsplash.com/600x600/?person";
+}
+
+class WA {
+  // For help
+  static String waUrl = "https://wa.me/";
+  static String getHelp(String number, String message) {
+    return waUrl + number + "/?text=${Uri.parse(message)}";
+  }
 }
 
 class API {
@@ -13,7 +22,11 @@ class API {
     return http.get(Uri.parse(BaseUrl.board + "/" + $id));
   }
 
-  static Uri log = Uri.parse(BaseUrl.log);
+  static Map<String, String>? headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+  };
+
+  static Uri login = Uri.parse(BaseUrl.login);
   static Uri board = Uri.parse(BaseUrl.board);
-  static Uri sensor = Uri.parse(BaseUrl.sensor);
 }
