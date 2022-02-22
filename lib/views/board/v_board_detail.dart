2ff9758adf6feae4790556fa21b0a616b4e9a128
Call Vin <kanggara75@gmail.com>
2022-02-22 22:31:09 +0700

    fix: move controller board detail to controler file

--- lib/views/board/v_board_detail.dart
+++ lib/views/board/v_board_detail.dart
@@ -1,10 +1,6 @@
-// ignore_for_file: avoid_print
-
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:fire_app/constant/url.dart';
 import 'package:timeago/timeago.dart' as timeago;
-import 'package:fire_app/models/m_board_detial.dart';
+import '../../controllers/c_board.dart';
 
 class BoardDetail extends StatefulWidget {
   final int id;
@@ -18,43 +14,20 @@ class BoardDetail extends StatefulWidget {
   _BoardDetailState createState() => _BoardDetailState();
 }
 
-class _BoardDetailState extends State<BoardDetail> {
-  // Membuat List Dari data Internet
-  List<BoardDetailModel> listModel = [];
-  var loading = false;
-
-  Future<void> getData() async {
-    setState(() {
-      loading = true;
-    });
-
-    final responseData = await API.getBoardDetail(widget.id.toString());
-
-    if (responseData.statusCode == 200) {
-      final data = jsonDecode(responseData.body);
-      print(data);
-      setState(() {
-        for (Map<String, dynamic> i in data) {
-          listModel.add(BoardDetailModel.fromJson(i));
-        }
-        loading = false;
-      });
-    }
-  }
-
-  //Panggil Data / Call Data
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
+class _BoardDetailState extends DetailController {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page List User'),
+        backgroundColor: Colors.lightBlue,
         centerTitle: true,
+        title: const Text(
+          'User Sensor List',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
       ),
       body: Container(
         child: loading
@@ -64,32 +37,142 @@ class _BoardDetailState extends State<BoardDetail> {
                 itemBuilder: (context, i) {
                   final sensorList = listModel[i];
                   return Card(
-                    color: Colors.amber[100],
+                    color: const Color(0xFF94D2BD),
                     margin: const EdgeInsets.all(15),
                     child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            sensorList.ruangan,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.green),
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.25,
+                                child: FittedBox(
+                                  child: Text(
+                                    sensorList.ruangan ?? "Unknown",
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(timeago.format(
-                            sensorList.createdAt,
-                            locale: 'id',
-                            allowFromNow: true,
-                          )),
-                          Text(timeago.format(
-                            sensorList.updatedAt,
-                            locale: 'id',
-                            allowFromNow: true,
-                          )),
+                          const Divider(
+                            thickness: 1,
+                            color: Colors.black,
+                          ),
                           const SizedBox(
                             height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                flex: 1,
+                                child: FittedBox(
+                                  fit: BoxFit.fitWidth,
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        'Temperatur',
+                                        style: TextStyle(
+                                          fontStyle: FontStyle.italic,
+                                          fontSize: 18,
+                                          color: (sensorList.temp >= 50
+                                              ? Colors.white
+                                              : Colors.black),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        sensorList.temp.toString() + '°C',
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 35,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            'Sensor Asap :   ',
+                                            style: TextStyle(
+                                              fontStyle: FontStyle.italic,
+                                              fontSize: 12,
+                                              color: (sensorList.temp >= 50
+                                                  ? Colors.white
+                                                  : Colors.black),
+                                            ),
+                                          ),
+                                          Text(
+                                            sensorList.notif ?? "Unknown",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                              color: (sensorList.temp >= 50
+                                                  ? Colors.amber
+                                                  : Colors.black),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Flexible(
+                                flex: 1,
+                                child: Column(
+                                  children: <Widget>[
+                                    const Text(
+                                      'Kelembapan',
+                                      style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontSize: 18,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      sensorList.humidity.toString() + ' %',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 35,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      timeago.format(
+                                        sensorList.updatedAt,
+                                        locale: 'id',
+                                        allowFromNow: true,
+                                      ),
+                                      style: const TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
