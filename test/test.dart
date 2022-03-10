925f73c24e921d5c8f9ae24a4ec446ce548f6137
Call Vin <kanggara75@gmail.com>
2022-03-10 15:37:42 +0700

    migrate new mac

---
+++ test/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+void main() => hello();
+
+void hello() {
+  if (kDebugMode) {
+    print("Hello\n");
+    print("Welcome to My LinkedIn Profile \n");
+    print("Let's Connect!");
+  }
+}
