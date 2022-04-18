e3ec8e760fc7b03f8b3dbd06f6a10aa5e830bd2f
Call Vin <kanggara75@gmail.com>
2022-04-19 05:45:31 +0700

    fix: format code with nvim

--- api/app/Http/Controllers/LogController.php
+++ api/app/Http/Controllers/LogController.php
@@ -19,13 +19,13 @@ class LogController extends Controller
       $otemp = $suhu->temp_operator;
       $ohumi = $suhu->humi_operator;
       $data = Log::where('temp', $otemp, $temp)
-                  ->orWhere('humidity', $ohumi, $humi)
-                  ->orWhere('mq2', $omq2, $mq2)
-                  ->orderBy('id', 'desc')
-                  ->get();
+        ->orWhere('humidity', $ohumi, $humi)
+        ->orWhere('mq2', $omq2, $mq2)
+        ->orderBy('id', 'desc')
+        ->get();
       return response()->json($data, 200, [], JSON_NUMERIC_CHECK);
     } else {
-      return response()->json(["messege" =>"No Setting Content"], 404);
+      return response()->json(["messege" => "No Setting Content"], 404);
     }
   }
 
@@ -33,34 +33,32 @@ class LogController extends Controller
   {
     $suhu = Setting::get()->last();
     $board = Board::where('id', '=', $id)->first();
-    if (isset($board))
-    {
+    if (isset($board)) {
       $mq2 = $suhu->mq2;
       $temp = $suhu->temperature;
       $omq2 = $suhu->mq2_operator;
       $otemp = $suhu->temp_operator;
       $sensor = Log::where('board_id', '=', $id)
-                    ->where(
-                      function ($query) use ($otemp, $temp, $omq2, $mq2) {
-                        $query->where('temp', $otemp, $temp)
-                              ->orWhere('mq2', $omq2, $mq2);
-                      }
-                    );
+        ->where(
+          function ($query) use ($otemp, $temp, $omq2, $mq2) {
+            $query->where('temp', $otemp, $temp)
+              ->orWhere('mq2', $omq2, $mq2);
+          }
+        );
       $data = $sensor->orderBy('id', 'desc')->get();
       $fcm = $sensor->first();
-      if (isset($fcm))
-      {
+      if (isset($fcm)) {
         fcm()
-        ->toTopic('api')
-        ->priority('high')
-        ->timeToLive(0)
-        ->notification([
-          'title' => $fcm->notif,
-          'body' => 'Lokasi ' . $fcm->ruangan . ' Di ' . $board->name,
-          'sound' => 'default',
+          ->toTopic('api')
+          ->priority('high')
+          ->timeToLive(0)
+          ->notification([
+            'title' => $fcm->notif,
+            'body' => 'Lokasi ' . $fcm->ruangan . ' Di ' . $board->name,
+            'sound' => 'default',
           ])
-        ->send();
-        }
+          ->send();
+      }
     } else {
       $data = [];
     }
